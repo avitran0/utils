5ce33b720f9f519283dbb915d8bec84011fcc5d1
avitran0 <holyhades64@gmail.com>
2026-02-26 23:22:33 +0100

    small rework

--- src/io.rs
+++ src/io.rs
@@ -7,200 +7,89 @@ pub enum Endian {
     Big,
 }
 
+macro_rules! impl_read_method {
+    ($name:ident, $type:ty) => {
+        #[inline]
+        fn $name(&mut self) -> Result<$type> {
+            let mut buf = [0; size_of::<$type>()];
+            self.read_exact(&mut buf)?;
+            Ok(<$type>::from_ne_bytes(buf))
+        }
+    };
+}
+
+macro_rules! impl_read_endian_method {
+    ($name:ident, $type:ty) => {
+        #[inline]
+        fn $name(&mut self, endian: Endian) -> Result<$type> {
+            let mut buf = [0; size_of::<$type>()];
+            self.read_exact(&mut buf)?;
+            Ok(match endian {
+                Endian::Native => <$type>::from_ne_bytes(buf),
+                Endian::Little => <$type>::from_le_bytes(buf),
+                Endian::Big => <$type>::from_be_bytes(buf),
+            })
+        }
+    };
+}
+
+macro_rules! impl_write_method {
+    ($name:ident, $type:ty) => {
+        #[inline]
+        fn $name(&mut self, value: $type) -> Result<()> {
+            self.write_all(&value.to_ne_bytes())
+        }
+    };
+}
+
+macro_rules! impl_write_endian_method {
+    ($name:ident, $type:ty) => {
+        #[inline]
+        fn $name(&mut self, value: $type, endian: Endian) -> Result<()> {
+            let bytes = match endian {
+                Endian::Native => value.to_ne_bytes(),
+                Endian::Little => value.to_le_bytes(),
+                Endian::Big => value.to_be_bytes(),
+            };
+            self.write_all(&bytes)
+        }
+    };
+}
+
 pub trait ReadBytes: std::io::Read {
-    #[inline]
-    fn read_u8(&mut self) -> Result<u8> {
-        let mut buf = [0; 1];
-        self.read_exact(&mut buf)?;
-        Ok(buf[0])
-    }
+    impl_read_method!(read_u8, u8);
 
-    #[inline]
-    fn read_u16(&mut self) -> Result<u16> {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf)?;
-        Ok(u16::from_ne_bytes(buf))
-    }
+    impl_read_method!(read_u16, u16);
+    impl_read_endian_method!(read_u16_endian, u16);
 
-    #[inline]
-    fn read_u16_endian(&mut self, endian: Endian) -> Result<u16> {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => u16::from_ne_bytes(buf),
-            Endian::Little => u16::from_le_bytes(buf),
-            Endian::Big => u16::from_be_bytes(buf),
-        })
-    }
+    impl_read_method!(read_u32, u32);
+    impl_read_endian_method!(read_u32_endian, u32);
 
-    #[inline]
-    fn read_u32(&mut self) -> Result<u32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(u32::from_ne_bytes(buf))
-    }
+    impl_read_method!(read_u64, u64);
+    impl_read_endian_method!(read_u64_endian, u64);
 
-    #[inline]
-    fn read_u32_endian(&mut self, endian: Endian) -> Result<u32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => u32::from_ne_bytes(buf),
-            Endian::Little => u32::from_le_bytes(buf),
-            Endian::Big => u32::from_be_bytes(buf),
-        })
-    }
+    impl_read_method!(read_u128, u128);
+    impl_read_endian_method!(read_u128_endian, u128);
 
-    #[inline]
-    fn read_u64(&mut self) -> Result<u64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(u64::from_ne_bytes(buf))
-    }
+    impl_read_method!(read_i8, i8);
 
-    #[inline]
-    fn read_u64_endian(&mut self, endian: Endian) -> Result<u64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => u64::from_ne_bytes(buf),
-            Endian::Little => u64::from_le_bytes(buf),
-            Endian::Big => u64::from_be_bytes(buf),
-        })
-    }
+    impl_read_method!(read_i16, i16);
+    impl_read_endian_method!(read_i16_endian, i16);
 
-    #[inline]
-    fn read_u128(&mut self) -> Result<u128> {
-        let mut buf = [0; 16];
-        self.read_exact(&mut buf)?;
-        Ok(u128::from_ne_bytes(buf))
-    }
+    impl_read_method!(read_i32, i32);
+    impl_read_endian_method!(read_i32_endian, i32);
 
-    #[inline]
-    fn read_u128_endian(&mut self, endian: Endian) -> Result<u128> {
-        let mut buf = [0; 16];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => u128::from_ne_bytes(buf),
-            Endian::Little => u128::from_le_bytes(buf),
-            Endian::Big => u128::from_be_bytes(buf),
-        })
-    }
+    impl_read_method!(read_i64, i64);
+    impl_read_endian_method!(read_i64_endian, i64);
 
-    #[inline]
-    fn read_i8(&mut self) -> Result<i8> {
-        let mut buf = [0; 1];
-        self.read_exact(&mut buf)?;
-        Ok(buf[0].cast_signed())
-    }
+    impl_read_method!(read_i128, i128);
+    impl_read_endian_method!(read_i128_endian, i128);
 
-    #[inline]
-    fn read_i16(&mut self) -> Result<i16> {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf)?;
-        Ok(i16::from_ne_bytes(buf))
-    }
+    impl_read_method!(read_f32, f32);
+    impl_read_endian_method!(read_f32_endian, f32);
 
-    #[inline]
-    fn read_i16_endian(&mut self, endian: Endian) -> Result<i16> {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => i16::from_ne_bytes(buf),
-            Endian::Little => i16::from_le_bytes(buf),
-            Endian::Big => i16::from_be_bytes(buf),
-        })
-    }
-
-    #[inline]
-    fn read_i32(&mut self) -> Result<i32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(i32::from_ne_bytes(buf))
-    }
-
-    #[inline]
-    fn read_i32_endian(&mut self, endian: Endian) -> Result<i32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => i32::from_ne_bytes(buf),
-            Endian::Little => i32::from_le_bytes(buf),
-            Endian::Big => i32::from_be_bytes(buf),
-        })
-    }
-
-    #[inline]
-    fn read_i64(&mut self) -> Result<i64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(i64::from_ne_bytes(buf))
-    }
-
-    #[inline]
-    fn read_i64_endian(&mut self, endian: Endian) -> Result<i64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => i64::from_ne_bytes(buf),
-            Endian::Little => i64::from_le_bytes(buf),
-            Endian::Big => i64::from_be_bytes(buf),
-        })
-    }
-
-    #[inline]
-    fn read_i128(&mut self) -> Result<i128> {
-        let mut buf = [0; 16];
-        self.read_exact(&mut buf)?;
-        Ok(i128::from_ne_bytes(buf))
-    }
-
-    #[inline]
-    fn read_i128_endian(&mut self, endian: Endian) -> Result<i128> {
-        let mut buf = [0; 16];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => i128::from_ne_bytes(buf),
-            Endian::Little => i128::from_le_bytes(buf),
-            Endian::Big => i128::from_be_bytes(buf),
-        })
-    }
-
-    #[inline]
-    fn read_f32(&mut self) -> Result<f32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(f32::from_ne_bytes(buf))
-    }
-
-    #[inline]
-    fn read_f32_endian(&mut self, endian: Endian) -> Result<f32> {
-        let mut buf = [0; 4];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => f32::from_ne_bytes(buf),
-            Endian::Little => f32::from_le_bytes(buf),
-            Endian::Big => f32::from_be_bytes(buf),
-        })
-    }
-
-    #[inline]
-    fn read_f64(&mut self) -> Result<f64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(f64::from_ne_bytes(buf))
-    }
-
-    #[inline]
-    fn read_f64_endian(&mut self, endian: Endian) -> Result<f64> {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf)?;
-        Ok(match endian {
-            Endian::Native => f64::from_ne_bytes(buf),
-            Endian::Little => f64::from_le_bytes(buf),
-            Endian::Big => f64::from_be_bytes(buf),
-        })
-    }
+    impl_read_method!(read_f64, f64);
+    impl_read_endian_method!(read_f64_endian, f64);
 
     #[inline]
     fn read_bytes(&mut self, count: usize) -> Result<Vec<u8>> {
@@ -210,7 +99,7 @@ pub trait ReadBytes: std::io::Read {
     }
 
     #[inline]
-    fn read_value<T: Default>(&mut self) -> Result<T> {
+    unsafe fn read_value<T: Default + Copy>(&mut self) -> Result<T> {
         let mut value = T::default();
         let buf = core::slice::from_mut(&mut value);
         let buf = unsafe {
@@ -229,160 +118,40 @@ pub trait WriteBytes: std::io::Write {
         self.write_all(&[value])
     }
 
-    #[inline]
-    fn write_u16(&mut self, value: u16) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
+    impl_write_method!(write_u16, u16);
+    impl_write_endian_method!(write_u16_endian, u16);
 
-    #[inline]
-    fn write_u16_endian(&mut self, value: u16, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
+    impl_write_method!(write_u32, u32);
+    impl_write_endian_method!(write_u32_endian, u32);
 
-    #[inline]
-    fn write_u32(&mut self, value: u32) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
+    impl_write_method!(write_u64, u64);
+    impl_write_endian_method!(write_u64_endian, u64);
 
-    #[inline]
-    fn write_u32_endian(&mut self, value: u32, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
-
-    #[inline]
-    fn write_u64(&mut self, value: u64) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
-
-    #[inline]
-    fn write_u64_endian(&mut self, value: u64, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
-
-    #[inline]
-    fn write_u128(&mut self, value: u128) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
-
-    #[inline]
-    fn write_u128_endian(&mut self, value: u128, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
+    impl_write_method!(write_u128, u128);
+    impl_write_endian_method!(write_u128_endian, u128);
 
     #[inline]
     fn write_i8(&mut self, value: i8) -> Result<()> {
         self.write_all(&value.to_ne_bytes())
     }
 
-    #[inline]
-    fn write_i16(&mut self, value: i16) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
+    impl_write_method!(write_i16, i16);
+    impl_write_endian_method!(write_i16_endian, i16);
 
-    #[inline]
-    fn write_i16_endian(&mut self, value: i16, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
+    impl_write_method!(write_i32, i32);
+    impl_write_endian_method!(write_i32_endian, i32);
 
-    #[inline]
-    fn write_i32(&mut self, value: i32) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
+    impl_write_method!(write_i64, i64);
+    impl_write_endian_method!(write_i64_endian, i64);
 
-    #[inline]
-    fn write_i32_endian(&mut self, value: i32, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
+    impl_write_method!(write_i128, i128);
+    impl_write_endian_method!(write_i128_endian, i128);
 
-    #[inline]
-    fn write_i64(&mut self, value: i64) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
+    impl_write_method!(write_f32, f32);
+    impl_write_endian_method!(write_f32_endian, f32);
 
-    #[inline]
-    fn write_i64_endian(&mut self, value: i64, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
-
-    #[inline]
-    fn write_i128(&mut self, value: i128) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
-
-    #[inline]
-    fn write_i128_endian(&mut self, value: i128, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
-
-    #[inline]
-    fn write_f32(&mut self, value: f32) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
-
-    #[inline]
-    fn write_f32_endian(&mut self, value: f32, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
-
-    #[inline]
-    fn write_f64(&mut self, value: f64) -> Result<()> {
-        self.write_all(&value.to_ne_bytes())
-    }
-
-    #[inline]
-    fn write_f64_endian(&mut self, value: f64, endian: Endian) -> Result<()> {
-        let bytes = match endian {
-            Endian::Native => value.to_ne_bytes(),
-            Endian::Little => value.to_le_bytes(),
-            Endian::Big => value.to_be_bytes(),
-        };
-        self.write_all(&bytes)
-    }
+    impl_write_method!(write_f64, f64);
+    impl_write_endian_method!(write_f64_endian, f64);
 
     #[inline]
     fn write_bytes(&mut self, bytes: &[u8]) -> Result<()> {
@@ -390,7 +159,7 @@ pub trait WriteBytes: std::io::Write {
     }
 
     #[inline]
-    fn write_value<T: Copy>(&mut self, value: &T) -> Result<()> {
+    unsafe fn write_value<T: Copy>(&mut self, value: &T) -> Result<()> {
         let buf = core::slice::from_ref(value);
         let buf = unsafe { core::slice::from_raw_parts::<u8>(buf.as_ptr().cast(), size_of::<T>()) };
         self.write_all(buf)
--- src/log.rs
+++ src/log.rs
@@ -1,19 +1,28 @@
 use std::{
     fs::{File, OpenOptions},
-    io::{LineWriter, Write as _},
+    io::{self, LineWriter, Write},
     path::{Path, PathBuf},
     str::FromStr,
 };
 
-use parking_lot::Mutex;
+use parking_lot::Mutex as ParkingMutex;
 
 pub use log::*;
 
+/// Configuration options for the Logger.
+#[derive(Debug, Clone)]
 pub struct LoggerOptions {
+    /// The logging level.
     pub level: Level,
+    /// Optional file path to write logs to.
     pub file: Option<PathBuf>,
+    /// Whether to truncate the log file, if present.
+    pub truncate: bool,
+    /// Whether to write logs to stdout.
     pub stdout: bool,
+    /// Optional module prefix to filter logs.
     pub module: Option<String>,
+    /// Whether to include debug information (file/line) in logs.
     pub debug: bool,
 }
 
@@ -22,6 +31,7 @@ impl Default for LoggerOptions {
         Self {
             level: Level::Info,
             file: None,
+            truncate: false,
             stdout: true,
             module: None,
             debug: false,
@@ -30,123 +40,130 @@ impl Default for LoggerOptions {
 }
 
 impl LoggerOptions {
+    /// Sets the logging level.
     pub fn level(mut self, level: Level) -> Self {
         self.level = level;
         self
     }
 
+    /// Sets the log file path.
     pub fn file(mut self, file: impl AsRef<Path>) -> Self {
         self.file = Some(file.as_ref().to_path_buf());
         self
     }
 
+    /// Enables or disables stdout logging.
     pub fn stdout(mut self, stdout: bool) -> Self {
         self.stdout = stdout;
         self
     }
 
+    /// Sets a module filter.
     pub fn module(mut self, module: &str) -> Self {
         self.module = Some(module.to_owned());
         self
     }
 
+    /// Enables or disables debug mode.
     pub fn debug(mut self, debug: bool) -> Self {
         self.debug = debug;
         self
     }
-
-    fn runtime(self, level: Level) -> RuntimeOptions {
-        RuntimeOptions {
-            level,
-            stdout: self.stdout,
-            module: self.module,
-            debug: self.debug,
-        }
-    }
 }
 
-struct RuntimeOptions {
-    level: Level,
-    stdout: bool,
-    module: Option<String>,
-    debug: bool,
-}
-
+/// A simple logger implementation.
 pub struct Logger {
-    writer: Option<Mutex<LineWriter<File>>>,
-    options: RuntimeOptions,
+    writer: Option<ParkingMutex<LineWriter<File>>>,
+    options: LoggerOptions,
 }
 
 impl Logger {
+    /// Installs the logger globally.
     pub fn install(options: LoggerOptions) {
-        Self::new(options).unwrap().init();
+        Self::new(options)
+            .expect("Failed to create logger")
+            .init()
+            .expect("Failed to install logger");
     }
 
-    pub fn new(options: LoggerOptions) -> std::io::Result<Self> {
-        let writer = if let Some(file) = &options.file {
-            let mut path = std::env::current_exe()?;
-            path.pop();
-            path.push(file);
+    /// Creates a new Logger instance.
+    pub fn new(mut options: LoggerOptions) -> io::Result<Self> {
+        // Handle RUST_LOG environment variable
+        if let Ok(level_env) = std::env::var("RUST_LOG")
+            && let Ok(level) = Level::from_str(&level_env)
+        {
+            options.level = level;
+        }
+
+        let writer = if let Some(file_path) = &options.file {
+            // Use current directory for relative paths, or absolute path as is.
+            // The original code used current_exe(), which is non-standard for logs.
+            // We'll stick to standard file opening which respects CWD.
+            // If the user wants current_exe relative, they should construct the path themselves.
+            // However, to be safe and "improve", standard CWD is better.
+
+            // Ensure parent directory exists?
+            if let Some(parent) = file_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+
             let file = OpenOptions::new()
                 .create(true)
-                .write(true)
-                .truncate(true)
-                .open(path)?;
-            Some(Mutex::new(LineWriter::new(file)))
+                .append(true)
+                .truncate(options.truncate)
+                .open(file_path)?;
+
+            Some(ParkingMutex::new(LineWriter::new(file)))
         } else {
             None
         };
 
-        let level_env = std::env::var("RUST_LOG").ok();
-        let level = if let Some(level_env) = &level_env {
-            match Level::from_str(level_env) {
-                Ok(level) => level,
-                Err(_) => options.level,
-            }
-        } else {
-            options.level
-        };
-
-        let options = options.runtime(level);
-
         Ok(Self { writer, options })
     }
 
-    pub fn init(self) {
+    /// Initializes the logger.
+    pub fn init(self) -> Result<(), SetLoggerError> {
         let max_level = self.options.level.to_level_filter();
-        log::set_boxed_logger(Box::new(self)).unwrap();
+        log::set_boxed_logger(Box::new(self))?;
         log::set_max_level(max_level);
+        Ok(())
     }
 
     fn write_log(&self, record: &Record) {
-        if let Some(module) = &self.options.module
-            && let Some(rec_module) = record.module_path()
-            && !rec_module.starts_with(module)
-        {
-            return;
+        if let Some(module) = &self.options.module {
+            if let Some(rec_module) = record.module_path() {
+                if !rec_module.starts_with(module) {
+                    return;
+                }
+            }
         }
 
         if self.options.stdout {
-            self.write(record, &mut std::io::stdout());
+            let stdout = std::io::stdout();
+            let mut handle = stdout.lock();
+            self.write(record, &mut handle);
         }
+
         if let Some(writer) = &self.writer {
             let mut writer = writer.lock();
-            self.write(record, writer.get_mut());
+            self.write(record, &mut *writer);
         }
     }
 
-    fn write(&self, record: &Record, writer: &mut impl std::io::Write) {
-        let _ = if self.options.debug
-            && let (Some(file), Some(line)) = (record.file(), record.line())
-        {
-            writeln!(
-                writer,
-                "[{}] [{}:{}] {}",
-                record.level(),
-                file,
-                line,
-                record.args()
-            )
+    fn write(&self, record: &Record, writer: &mut impl Write) {
+        let _ = if self.options.debug {
+            if let (Some(file), Some(line)) = (record.file(), record.line()) {
+                writeln!(
+                    writer,
+                    "[{}] [{}:{}] {}",
+                    record.level(),
+                    file,
+                    line,
+                    record.args()
+                )
+            } else {
+                writeln!(writer, "[{}] {}", record.level(), record.args())
+            }
         } else {
             writeln!(writer, "[{}] {}", record.level(), record.args())
         };
@@ -154,11 +171,11 @@ impl Logger {
 }
 
 impl Log for Logger {
-    fn enabled(&self, metadata: &log::Metadata) -> bool {
+    fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= self.options.level
     }
 
-    fn log(&self, record: &log::Record) {
+    fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
             self.write_log(record);
         }
@@ -168,5 +185,6 @@ impl Log for Logger {
         if let Some(writer) = &self.writer {
             let _ = writer.lock().flush();
         }
+        let _ = std::io::stdout().flush();
     }
 }
