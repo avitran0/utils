4530942df7258093f43603aed29b1cd92a2f3fb5
avitran0 <holyhades64@gmail.com>
2026-04-09 13:32:39 +0200

    add feature gates

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,15 +1,36 @@
 //! utility types and traits built on top of the standard library.
 
+#[cfg(feature = "bitset")]
 pub use bitset;
+
+#[cfg(feature = "channel")]
 pub use channel;
+
+#[cfg(feature = "future")]
 pub use future;
+
+#[cfg(feature = "io")]
 pub use io;
+
+#[cfg(feature = "log")]
 pub use log;
-#[cfg(target_os = "linux")]
+
+#[cfg(all(feature = "meta", target_os = "linux"))]
 pub use meta;
-#[cfg(target_os = "linux")]
+
+#[cfg(all(feature = "path", target_os = "linux"))]
 pub use path;
+
 /// re-exports synchronization primitives from `parking_lot`.
+#[cfg(feature = "sync")]
 pub mod sync {
     pub use parking_lot::{Mutex, RwLock};
+}
+
+pub const fn is_debug() -> bool {
+    cfg!(debug_assertions)
+}
+
+pub const fn is_test() -> bool {
+    cfg!(test)
 }
