4e432dae6683daa4e8485fe5403308217bb6ecef
avitran0 <holyhades64@gmail.com>
2026-04-10 09:04:12 +0200

    remove bogus test

--- src/meta.rs
+++ src/meta.rs
@@ -18,18 +18,3 @@ macro_rules! embed_metadata {
         static $name: [u8; $value.len()] = *$value;
     };
 }
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn embed_metadata() {
-        embed_metadata!(TEST_META, ".test_section", &[1, 2, 3, 4]);
-
-        assert_eq!(TEST_META, [1, 2, 3, 4]);
-
-        let ptr = TEST_META.as_ptr();
-        unsafe {
-            assert_eq!(*ptr, 1);
-        }
-    }
-}
